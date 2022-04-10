b41bf3a097b0fc7044f34bae2d53838e44028b96
Eric Hoover <ehoover3@yahoo.com>
2022-04-10 12:18:59 -0700

    completed first draft

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import * as React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 
 export default function App() {
   return (
-    <>
+    <div className='App'>
       <Navbar />
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='portfolio' element={<Portfolio />} />
-        </Routes>
-      </Router>
-    </>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='portfolio' element={<Portfolio />} />
+      </Routes>
+    </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
-    <div>
+    <div style={{ display: "flex", justifyContent: "space-around" }}>
       <Link to='/'>Home Page</Link>
-      <Link to='/'>Home Page</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/portfolio'>Portfolio</Link>
     </div>
   );
 }
